abc073ec18af3991727b18fe0946599acb09d158
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2024-09-09 16:41:35 +0100

    added logout to sidebar

--- src/layouts/dashboard/side-nav.js
+++ src/layouts/dashboard/side-nav.js
@@ -1,8 +1,12 @@
-import { Box, Divider, Drawer, Stack, useMediaQuery } from '@mui/material';
+import { Logout } from '@mui/icons-material';
+import { Box, ButtonBase, Divider, Drawer, Stack, useMediaQuery } from '@mui/material';
 import Image from 'next/image';
 import NextLink from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
+import { useCallback } from 'react';
+import toast from 'react-hot-toast';
 import { useAuth } from 'src/hooks/use-auth';
 import { items } from './config';
 import { SideNavItem } from './side-nav-item';
@@ -12,7 +16,7 @@ export const SideNav = (props) => {
   const { open, onClose } = props;
   const pathname = usePathname();
   const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
   const userAccountType = user?.account_type;
 
   const content = (
@@ -84,6 +88,7 @@ export const SideNav = (props) => {
               />
             );
           })}
+          <LogoutButton />
         </Stack>
       </Box>
       <Divider sx={{ borderColor: 'neutral.700' }} />
@@ -133,3 +138,74 @@ SideNav.propTypes = {
   onClose: PropTypes.func,
   open: PropTypes.bool,
 };
+
+function LogoutButton() {
+  const router = useRouter();
+  const { signOut } = useAuth();
+
+  const handleSignOut = useCallback(async () => {
+    try {
+      await toast.promise(signOut(), {
+        loading: 'Signing out',
+        success: () => {
+          router.push('/auth/login');
+          return 'Sign out successfull';
+        },
+        error: (error) => {
+          if (error.response.data.message === 'Sorry, you are not logged in') {
+            router.push('/auth/login');
+          }
+          return error?.response?.data?.message ?? error.message;
+        },
+      });
+    } catch (erro) {}
+  }, [signOut, router]);
+  return (
+    <li>
+      <ButtonBase
+        sx={{
+          alignItems: 'center',
+          borderRadius: 1,
+          display: 'flex',
+          justifyContent: 'flex-start',
+          pl: '16px',
+          pr: '16px',
+          py: '6px',
+          textAlign: 'left',
+          width: '100%',
+          '&:hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.04)',
+          },
+        }}
+        onClick={handleSignOut}
+      >
+        <Box
+          component="span"
+          sx={{
+            alignItems: 'center',
+            color: 'neutral.400',
+            display: 'inline-flex',
+            justifyContent: 'center',
+            mr: 2,
+          }}
+        >
+          <Logout />
+        </Box>
+        <Box
+          component="span"
+          sx={{
+            color: 'neutral.400',
+            flexGrow: 1,
+            fontFamily: (theme) => theme.typography.fontFamily,
+            fontSize: 14,
+            fontWeight: 600,
+            lineHeight: '24px',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          Logout
+        </Box>
+      </ButtonBase>
+    </li>
+  );
+}
